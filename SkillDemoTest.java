c9fd6fb3219a5f35d4d3f91ca6caedabc59c67e9
NateWick4 <103294546+NateWick4@users.noreply.github.com>
2022-05-01 21:50:58 -0700

    Added a test file

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testMult() {
+        assertEquals(2, SkillDemo.mult(2, 2));
+    }
+}
