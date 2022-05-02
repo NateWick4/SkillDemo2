74e457487230ef98eaecf7605d98add07e477b06
NateWick4 <103294546+NateWick4@users.noreply.github.com>
2022-05-01 21:53:29 -0700

    Fixed the JUnit test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testMult() {
-        assertEquals(2, SkillDemo.mult(2, 2));
+        assertEquals(2, SkillDemo.mult(1, 2));
     }
 }
